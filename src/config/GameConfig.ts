e8a27dfdda26b00ef2aa610d11d0911beee3d175
Hadik <hadikcze@gmail.com>
2022-10-01 17:14:13 +0200

    Art

--- src/config/GameConfig.ts
+++ src/config/GameConfig.ts
@@ -5,7 +5,8 @@ export default {
     PhaserBasicSettings: phaserBasicSettings,
     UI: ui,
     World: {
-        size: {
+        size: { // 	640	360 is ART -> double it
+            // 1280 x 720
             width: 1360,
             height: 768
         }
